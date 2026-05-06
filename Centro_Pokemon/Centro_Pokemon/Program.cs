75636cd78a5cad910f489dccb424427ba909c7a5
Enzo Santos Nascimento <enzosn25@gmail.com>
2026-05-06 13:55:47 -0300

    Centro Pokemon

--- Centro_Pokemon/Centro_Pokemon/Program.cs
+++ Centro_Pokemon/Centro_Pokemon/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Centro_Pokemon
@@ -11,18 +12,18 @@ namespace Centro_Pokemon
         static void Main(string[] args)
         {
 
-           int opcao = 0;
+            int opcao = 0;
             while (opcao != 4)
             {
 
-                Console.Clear();  //
-                Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();  
+            Console.ForegroundColor = ConsoleColor.Green;
 
 
 
 
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine(@"
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(@"
      ██████╗░░█████╗░██╗░░██╗███████╗███╗░░░███╗░█████╗░███╗░░██╗
      ██╔══██╗██╔══██╗██║░██╔╝██╔════╝████╗░████║██╔══██╗████╗░██║
      ██████╔╝██║░░██║█████═╝░█████╗░░██╔████╔██║██║░░██║██╔██╗██║
@@ -30,62 +31,106 @@ namespace Centro_Pokemon
      ██║░░░░░╚█████╔╝██║░╚██╗███████╗██║░╚═╝░██║╚█████╔╝██║░╚███║
      ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝");
 
-                Console.ResetColor();
-                Console.WriteLine("\n Seja muito bem-vindo ao Centro Pokemon!\n");
+            Console.ResetColor();
+            Console.WriteLine("\n Seja muito bem-vindo ao Centro Pokemon!\n");
 
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("\n 1 - Cadastrar Pokemon");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n 1 - Cadastrar Pokemon");
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\n 2 - Consultar Pokemon");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n 2 - Consultar Pokemon");
 
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("\n 3 - Apagar Pokemon");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n 3 - Apagar Pokemon");
 
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("\n 4 - Sair\n");
-                Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n 4 - Sair\n");
+            Console.ResetColor();
 
-                opcao = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n Digite a opção escolhida: \n");
+            Console.ResetColor();
 
-                switch (opcao)
-                {
+            opcao = int.Parse(Console.ReadLine());
 
-                    case 1:
+            switch (opcao) {
 
+            case 1:
+            CadastrarPokemon();
+            break;
 
-                        break;
-                    case 2:
+            case 2:
+            break;
 
+            case 3:
+            break;
 
-                        break;
-                    case 3:
+            case 4:
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("\nTchau Tchau, volte sempre!!!");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+            break;
 
+            default:
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\nOpção Inválida!!!");
+            Console.ResetColor();
+            System.Threading.Thread.Sleep(2000);
+            break;
 
-                        break;
-                    case 4:
-                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                        Console.WriteLine("\nTchau Tchau, volte sempre!!!");
-                        Console.ResetColor();
-                        System.Threading.Thread.Sleep(2000);
+         }
+       }
+     }
+            static void CadastrarPokemon() {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"
+     ░█████╗░░█████╗░██████╗░░█████╗░░██████╗████████╗██████╗░░█████╗░
+     ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+     ██║░░╚═╝███████║██║░░██║███████║╚█████╗░░░░██║░░░██████╔╝██║░░██║
+     ██║░░██╗██╔══██║██║░░██║██╔══██║░╚═══██╗░░░██║░░░██╔══██╗██║░░██║
+     ╚█████╔╝██║░░██║██████╔╝██║░░██║██████╔╝░░░██║░░░██║░░██║╚█████╔╝
+░     ╚════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░");
+                   
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n Digite o Nome do seu Pokemon: \n");
+            string nomePokemon = Console.ReadLine();
 
-                        break;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n Digite o Tipo do seu Pokemon: \n");
+            string  tipoPokemon = Console.ReadLine();
 
-                    default:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("\nOpção Inválida!!!");
-                        Console.ResetColor();
-                        System.Threading.Thread.Sleep(2000);
-                        break;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n Digite a Altura do seu Pokemon: \n");
+            string  alturaPokemon = Console.ReadLine();
 
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n Digite o Peso do seu Pokemon: \n");
+            string  pesoPokemon = Console.ReadLine();
+                      
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n Quantas Fraquezas ele tem? :\n");
+            int qtdFraqueza = int.Parse(Console.ReadLine());
 
-                }
+            for (int i = 1; i <= qtdFraqueza; i++) {
+            Console.WriteLine($"Fraqueza {i} : ");
+            string fraquezaPokemon = Console.ReadLine(); }
 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n Quantas Evoluções ele tem? : \n");
+            int qtdEvolucao = int.Parse(Console.ReadLine());
 
-
-
-
-            }
+            for (int i = 1; i <= qtdEvolucao; i++) {
+            Console.WriteLine($"Evolução {i} : \n"); }
+            string evolucaoPokemon = Console.ReadLine();
+                
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("\n Parabéns ... Pokemon Cadastrado ");
+            Thread.Sleep(3000);
+            Console.ResetColor();
+                
+           
         }
+      }
     }
-}
